c9df55a1f1538494f26ec3052745d0ab1b5dde86
Stefano <oddouu@gmail.com>
2020-05-17 15:21:57 +0100

    added circle to map

--- public/javascripts/static-location.js
+++ public/javascripts/static-location.js
@@ -1,5 +1,5 @@
 let staticLocation = $('#static-location');
-console.log(staticLocation)
+
 let lat = staticLocation[0].attributes.lat.value;
 let lng = staticLocation[0].attributes.lng.value;
 
@@ -11,4 +11,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
+}).addTo(mymap);
+
+
+var circle = L.circle([lat, lng], {
+  color: '#006a4e',
+  fillColor: '#b8d5cd',
+  fillOpacity: 0.5,
+  radius: 500
 }).addTo(mymap);
