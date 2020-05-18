e44b7ff93631f6289292c377ad21eb6f3bf521bc
Stefano <oddouu@gmail.com>
2020-05-18 01:04:11 +0100

    precompiling emails

---
+++ public/javascripts/dropdowns.js
@@ -0,0 +1,54 @@
+// firstDropdown for main categories
+let firstDropdown = $('#category');
+let secondDropdown = $('#subCategory');
+
+
+firstDropdown.empty();
+secondDropdown.empty();
+
+firstDropdown.append('<option selected="true" disabled>Choose Category</option>');
+firstDropdown.prop('selectedIndex', 0);
+
+secondDropdown.append('<option selected="true" disabled>Choose sub Category (optional)</option>');
+secondDropdown.prop('selectedIndex', 0);
+
+const url = '/categories.JSON';
+let subCategories = [];
+
+// Populate firstDropdown with list of categories
+$.getJSON(url, function (data) {
+  $.each(data, function (key, entry) {
+    if (entry.L2) {
+      subCategories.push(entry);
+    }
+
+    if (entry.L2 === undefined) {
+      firstDropdown
+        .append($('<option></option>')
+          .attr('value', entry.L1)
+          .text(entry.L1));
+    }
+
+  });
+});
+
+
+$(firstDropdown).change(function () {
+  const L2index = firstDropdown[0].value;
+  console.log("L2index", L2index);
+  //Populate secondDropdown with list of sub-categories
+  $.getJSON(url, function (data) {
+    $.each(data, function (key, entry) {
+      if (entry.L2 && L2index === entry.L1) {
+        console.log(L2index);
+        console.log(entry);
+        secondDropdown
+          .append($('<option></option>')
+            .attr('value', entry.L2)
+            .text(entry.L2));
+      }
+
+    });
+  });
+
+});
---
+++ public/javascripts/emailcompiler.js
@@ -0,0 +1,35 @@
+let pickUpDateInput = $('#pickup-date')
+let message = $('#message')
+
+$(document).ready(function () {
+  $('#message').summernote({
+    toolbar: [
+      // [groupName, [list of button]]
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']]
+    ],
+    placeholder: '',
+    height: 250,
+    minHeight: 100,
+    maxHeight: 450,
+  });
+
+  $('.notifications').summernote();
+});
+
+
+$(pickUpDateInput).change(function () {
+  let pickUpDate = pickUpDateInput[0].value;
+  let sendToField = $('#send-to');
+  let listingNameField = $('#listing-name');
+
+  console.log(listingNameField)
+
+  let listingName = listingNameField[0].attributes.listing.value;
+  let sendToName = sendToField[0].attributes.sendTo.value;
+  console.log(sendToName);
+  console.log(pickUpDate);
+  $('#message').text(`Hi ${sendToName}, I would like to come pick-up your ${listingName} on the ${pickUpDate}.`);
+
+});
